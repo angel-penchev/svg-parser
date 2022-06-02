32d768902f7413e1b93c050906e4db2994b384ba
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2022-06-02 17:50:57 +0300

    [Fix] Duplicate enum values across enums
    Apparently you cannot have the same enum values across enums.
    Another of the beautiful wonders of C++. ✨✨

--- lib/exception/HelperException.cpp
+++ lib/exception/HelperException.cpp
@@ -9,7 +9,7 @@ HelperException::HelperException(HelperErrorCode errorCode) : Exception(errorCod
 
 const char *HelperException::getErrorMessage() const {
     switch (this->errorCode) {
-        case HelperErrorCode::CANNOT_SET_NULLPTR_AS_VALUE:
+        case HelperErrorCode::CANNOT_SET_NULLPTR_AS_HELPER_VALUE:
             return "Cannot assign value from null pointer.";
         case HelperErrorCode::INDEX_OUT_OF_INDEX:
             return "Index specified is out of the array range.";
